6c373050b1b228b9e9c50ad02429274a13429377
Ahmed Dahmani <ahmeddahmani@issatso.u-sousse.tn>
2025-04-29 23:43:29 +0100

    Added NightAtTheMuseum problem

---
+++ NightAtTheMuseum/Program.cs
@@ -0,0 +1,17 @@
+﻿string word = Console.ReadLine();
+int pointer = 'a';
+int steps = 0; 
+for (int i = 0; i < word.Length; i++)
+{
+    int diff = Math.Abs(word[i] - pointer);
+    if (diff <= 13)
+    {
+        steps += diff;
+    }
+    else
+    {
+        steps += (26 - diff);
+    }
+    pointer = word[i];
+}
+Console.WriteLine(steps);
