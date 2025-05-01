9e804217ee782ee1423096ef80dca41c92206111
Ahmed Dahmani <ahmeddahmani@issatso.u-sousse.tn>
2025-05-01 12:53:32 +0100

    inital commit

---
+++ NormalProblem/Program.cs
@@ -0,0 +1,13 @@
+﻿int t = int.Parse(Console.ReadLine());
+for (int i = 0; i < t; i++)
+{
+    string a = Console.ReadLine();
+    char[] b = new char[a.Length];
+    for (int j = 0; j < a.Length; j++)
+    {
+        if (a[j] == 'p') b[j] = 'q';
+        else if (a[j] == 'q') b[j] = 'p';
+        else b[j] = 'w';
+    }
+    Console.WriteLine(new string(b));
+}
