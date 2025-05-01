e649d01577489215e9a6bad411cfaf2b6eea5d7d
Ahmed Dahmani <ahmeddahmani@issatso.u-sousse.tn>
2025-05-01 12:55:15 +0100

    fixed failing on some test cases

--- NormalProblem/Program.cs
+++ NormalProblem/Program.cs
@@ -5,9 +5,9 @@ for (int i = 0; i < t; i++)
     char[] b = new char[a.Length];
     for (int j = 0; j < a.Length; j++)
     {
-        if (a[j] == 'p') b[j] = 'q';
-        else if (a[j] == 'q') b[j] = 'p';
-        else b[j] = 'w';
+        if (a[j] == 'p') b[a.Length - j - 1] = 'q';
+        else if (a[j] == 'q') b[a.Length - j - 1] = 'p';
+        else b[a.Length - j - 1] = 'w';
     }
     Console.WriteLine(new string(b));
 }
