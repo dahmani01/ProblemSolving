4b775f5cd3511679b13f81214164598f1af26aa7
Ahmed Dahmani <ahmeddahmani@issatso.u-sousse.tn>
2025-05-01 13:58:53 +0100

    Solved Translation problem

---
+++ Translation/Program.cs
@@ -0,0 +1,18 @@
+﻿string s = Console.ReadLine();
+string t = Console.ReadLine();
+if (s.Length != t.Length)
+{
+    Console.WriteLine("NO");
+    return; 
+}
+
+for (short i = 0; i < s.Length; i++)
+{
+    if (s[i] != t[s.Length - i -1])
+    {
+        Console.WriteLine("NO");
+        return;
+    }
+}
+
+Console.WriteLine("YES");
