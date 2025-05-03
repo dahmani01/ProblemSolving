10b15bac03a933a2f622db46345b39f7945b9cd4
Ahmed Dahmani <ahmeddahmani@issatso.u-sousse.tn>
2025-05-03 17:19:01 +0100

    Added stringTask problem

---
+++ StringTask/Program.cs
@@ -0,0 +1,11 @@
+﻿string input = Console.ReadLine().ToLower();
+char[] vowels = ['a', 'o', 'y', 'e', 'u', 'i'];
+
+List<string> word = new List<string>();
+foreach (var letter in input)
+{
+    if (!vowels.Contains(letter))
+        word.Add($".{letter}");
+}
+
+Console.WriteLine(string.Join("", word));
