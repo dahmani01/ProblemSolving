03539e0931bb0e9ca32e3b3375107651f4ebdb3a
Ahmed Dahmani <ahmeddahmani@issatso.u-sousse.tn>
2025-05-04 01:32:17 +0100

    Solved laptops problem

---
+++ Laptops/Program.cs
@@ -0,0 +1,31 @@
+﻿int n = int.Parse(Console.ReadLine());
+List<Laptop> laptops = new List<Laptop>();  
+
+for (var i = 0; i < n; i++)
+{
+    int[] input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+    int price = input[0];
+    int quality = input[1];
+    var laptop = new Laptop(price, quality);
+    laptops.Add(laptop);
+} 
+
+var sortedLaptops = laptops.OrderBy(laptop => laptop.Price).ToList();
+var index = 1;
+var MaxQuality = sortedLaptops[0].Quality;
+while (index < sortedLaptops.Count)
+{
+    if (sortedLaptops[index].Quality > MaxQuality)
+        MaxQuality = sortedLaptops[index].Quality;
+    else
+    {
+        Console.WriteLine("Happy Alex");
+        return;
+    }
+
+    index++;
+}
+
+Console.WriteLine("Poor Alex");
+public record Laptop(int Price , int Quality) ;
+
