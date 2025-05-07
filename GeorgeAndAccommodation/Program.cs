91d174ec9bf906c60d327bbf42db30ddb7dc220e
Ahmed Dahmani <ahmeddahmani@issatso.u-sousse.tn>
2025-05-07 21:42:08 +0100

    Solved Geroge and Accommodation problem

---
+++ GeorgeAndAccommodation/Program.cs
@@ -0,0 +1,9 @@
+﻿short n = short.Parse(Console.ReadLine());
+short numberOfRooms = 0;
+for (short i = 1; i <= n; i++)
+{
+    string[] input = Console.ReadLine().Split(" ");
+    short p = short.Parse(input[0]), q = short.Parse(input[1]);
+    if (q-p >= 2) numberOfRooms++;
+}
+Console.WriteLine(numberOfRooms);
