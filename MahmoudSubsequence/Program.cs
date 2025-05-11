6edb3496812038c119539d6477e1b8f3273eff83
Ahmed Dahmani <ahmeddahmani@issatso.u-sousse.tn>
2025-05-11 23:43:14 +0100

    solved uncommon long subsequence problem

---
+++ MahmoudSubsequence/Program.cs
@@ -0,0 +1,9 @@
+﻿string a = Console.ReadLine();
+string b = Console.ReadLine();
+
+if(a.Equals(b))
+    Console.WriteLine(-1);
+else if (a.Length < b.Length)
+    Console.WriteLine(b.Length);
+else 
+    Console.WriteLine(a.Length);
