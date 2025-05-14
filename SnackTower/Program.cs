56b1baf7f8d6b7a614df7cfdc516c7726f70cb9a
Ahmed Dahmani <ahmeddahmani@issatso.u-sousse.tn>
2025-05-15 00:20:12 +0100

    Solved snacktower problem

---
+++ SnackTower/Program.cs
@@ -0,0 +1,19 @@
+﻿int n = int.Parse(Console.ReadLine());
+var snackSizes = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+bool[] seen = new bool[n + 1];
+int nextExpected = n;
+
+for (int i = 0; i < n; i++) {
+    int s = snackSizes[i];
+    seen[s] = true;
+    
+    while (nextExpected >= 1 && seen[nextExpected]) {
+        Console.Write(nextExpected + " ");
+        nextExpected--;
+    }
+    Console.WriteLine();
+}
